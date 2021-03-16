e9ff6b0e500b1e79f92f5c8852488908ec9100a3
Neel <neelusenthil1996@gmail.com>
2021-03-16 09:43:03 +0530

    include complete build setup

--- gitconvex.rb
+++ gitconvex.rb
@@ -3,17 +3,23 @@ class Gitconvex < Formula
   homepage "https://gitconvex.com"
   url "https://github.com/neel1996/gitconvex/archive/2.1.0-beta-1.tar.gz"
   sha256 "0d26bfd5913373ab4c2f7864925c891321e48eaea8c1a39d7f9c81a2a00ee76f"
-  license "Apache-2.0 License"
+  license "Apache-2.0"
 
   depends_on "go" => :build
+  depends_on "node"
+  depends_on "make"
   depends_on "libssh2"
   depends_on "libgit2"
 
   def install
-    system "go", "build", *std_go_args
+    system "make", "build"
+    system "mkdir", "-p", "/usr/local/gitconvex"
+    system "cp", "./dist/*", "/usr/local/gitconvex"
+    system "ln", "-s", "/usr/local/gitconvex/gitconvex", "/usr/local/bin"
+    system "ln", "-s", "/usr/local/gitconvex/gitconvex-ui/", "/usr/local/bin"
   end
 
   test do
-    system "false"
+    assert_not_nil shell_output("gitconvex --version")
   end
 end
