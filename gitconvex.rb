3b973321efe162fe1daf05c1ae461941947c8d0f
Neel <neelusenthil1996@gmail.com>
2021-03-17 18:57:05 +0530

    add proper setup with linking instructions

--- gitconvex.rb
+++ gitconvex.rb
@@ -9,16 +9,38 @@ class Gitconvex < Formula
   depends_on "libssh2"
   depends_on "libgit2"
 
+  def print_lines(sym)
+    $i = 0
+    print "\n"
+    while $i < 50 do
+      print "#{sym}"
+      $i += 1
+    end
+    print "\n"
+  end
+
   def install
+    # Current version will be updated every release
+    version = "2.1.0"
+    system "cp", "-rp", "./dist/", bin
     ENV["GOPATH"] = buildpath
     bin_path = buildpath/"src/github.com/neel1996/gitconvex-server"
     bin_path.install Dir["*"]
     cd bin_path do
-      system "go", "build", "-v", "-a", "-o", bin/"gitconvex", "."
+      system "go", "build", "-v", "-o", bin/"gitconvex", "."
+      print_lines("=")
+      print "\n✅\tGitconvex server is successfully installed!"
+      print "\n🖥️\tNow copy and run the following command to link the UI directory to the same path"
+      print "\n"
+      cmd = "ln -s #{HOMEBREW_PREFIX}/Cellar/gitconvex/#{version}/bin/gitconvex-ui #{HOMEBREW_PREFIX}/bin/"
+      print_lines(">")
+      print "\n#{cmd}\n"
+      print_lines("<")
+      print_lines("=")
     end
   end
 
   test do
-    assert_match "v2.1.0", shell_output("#{bin}/gitconvex --version")
+    assert_match "2.1.0", shell_output("#{bin}/gitconvex --version")
   end
 end
